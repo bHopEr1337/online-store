85d0deb4de05c996259cce71d91cb0791cfe5067
Alex <pkuslin9@gmail.com>
2024-07-15 15:45:50 +0300

    pagination

--- frontMain/views.py
+++ frontMain/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from frontMain.models import Product
+from django.core.paginator import Paginator
 
 
 def index(request):
@@ -14,8 +15,13 @@ def contact(request):
     return render(request, template_name='frontMain/contact.html')
 
 def house(request):
+    post_list = Product.objects.all()
+    paginator = Paginator(post_list, 6)
+    page_number = request.GET.get('page', 1)
+    houses = paginator.page(page_number)
+
     return render(request, template_name='frontMain/house.html', context={
-        'products' : Product.objects.all()
+        'houses' : houses
     })
 
 def price(request):
